b958f052972b157b3978c62a7069689da2d568cf
Ihor Sokorchuk <ihor.sokorchuk@nure.ua>
2024-10-30 03:04:47 +0200

    Edited set-telegram-web-hook.sh

--- task5-3/set-telegram-web-hook.sh
+++ task5-3/set-telegram-web-hook.sh
@@ -1,6 +1,21 @@
 #! /bin/bash
 
+echo 'Увімкнути/вимкнути WebHook для Telegram бота'
+
 YOUR_BOT_TOKEN='YOUR_BOT_TOKEN'
 WEB_HOOK_URL='https://yourdomain.com/path/to/your-script.php'
 
-wget https://api.telegram.org/bot${YOUR_BOT_TOKEN}/setWebhook?url=$WEB_HOOK_URL
+select choise in 'Увімкнути WebHook' 'Вимкнути WebHook'; do
+  case "$choise" in
+    Увімк*) wget -X POST https://api.telegram.org/bot${YOUR_BOT_TOKEN}/setWebhook?url=$WEB_HOOK_URL || exit
+      echo "WebHook увімкнено на $WEB_HOOK_URL"
+      break
+      ;;
+    Вимкн*) wget -X POST https://api.telegram.org/bot${YOUR_BOT_TOKEN}/deleteWebhook || exit
+      echo 'WebHook вимкнено'
+      break
+      ;;
+  esac
+done
+
+# EOF
